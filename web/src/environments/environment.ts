aac365b0b3cdbc3356b01531563d966d9c1124de
Pedroso <vpedroso.dev@gmail.com>
2025-08-12 15:23:54 -0300

    chore: add envs

---
+++ web/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  problemsURL:
+    'https://raw.githubusercontent.com/pgvi/lc-patterns-aggregator/refs/heads/master/aggregator/data/output/leetcode_patterns_aggregate.json',
+  updatesURL:
+    'https://raw.githubusercontent.com/pgvi/lc-patterns-aggregator/refs/heads/master/aggregator/data/output/leetcode_update_dates.json',
+};
